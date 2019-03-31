96a9ade3d5ec3da02f019fa05afa6ad5b9c67b71
Cat <money1062@live.com>
2019-03-31 19:03:53 -0400

    spamming that config update.

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": MYSQL_PW,
+    "password": process.env.MYSQL_PW || MYSQL_PW,
     "database": "users_DB",
     "host": "127.0.0.1",
     "dialect": "mysql"
